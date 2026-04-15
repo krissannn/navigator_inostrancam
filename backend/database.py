32434359ce0aa174bd37bf5bdebe46751fd277e4
krissannn <dib547102@gmail.com>
2026-04-15 22:37:56 +0500

    убрала load_dotenv из database

--- backend/database.py
+++ backend/database.py
@@ -2,26 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-# Загружаем переменные из файла .env
-load_dotenv()
-
-# Получаем DATABASE_URL (для Render)
+# Получаем DATABASE_URL из переменных окружения Render
 database_url = os.getenv("DATABASE_URL")
 
-# Если нет DATABASE_URL — используем отдельные переменные (для локальной разработки)
 if not database_url:
+    # Для локальной разработки
     DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
     DB_NAME = os.getenv("DB_NAME", "navigator")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     
-    # Собираем строку подключения
     database_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
 
-# Используем ОДНУ переменную database_url
 engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
